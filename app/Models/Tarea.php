d9f164f31fafa53fa8533a0e07c72cad0893b1f1
Ramiro Sierra <ramiromateosierranunez@gmail.com>
2025-06-28 16:54:04 -0300

    feat:Creamos el modelo,  controlador y la migracion correspondietes a Tarea

---
+++ app/Models/Tarea.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Tarea extends Model
+{
+    /** @use HasFactory<\Database\Factories\TareaFactory> */
+    use HasFactory;
+}
