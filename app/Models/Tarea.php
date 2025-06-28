3913bcbc85569c35a0da4cf73afb618f4b1d3c76
Ramiro Sierra <ramiromateosierranunez@gmail.com>
2025-06-28 17:06:52 -0300

    feat: Programamos el modelo tarea

--- app/Models/Tarea.php
+++ app/Models/Tarea.php
@@ -1,12 +1,27 @@
 <?php
 
-namespace App;
+namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Tarea extends Model
 {
-    /** @use HasFactory<\Database\Factories\TareaFactory> */
-    use HasFactory;
+    use SoftDeletes;
+
+    protected $fillable = [
+        'titulo',
+        'autor_id',
+        'asignado_id',
+        'cuerpo',
+        'fecha_expiracion',
+        'categorias',
+        'comentarios'
+    ];
+
+    protected $casts = [
+        'categorias' => 'array',
+        'comentarios' => 'array',
+        'fecha_expiracion' => 'datetime'
+    ];
 }
